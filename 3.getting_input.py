86f8dcd8cd1b8db4728a0a863a55a3aecbea4628
xtremist96 <riz1.xtreme@gmail.com>
2022-07-05 14:00:36 +0530

    Input

---
+++ 3.getting_input.py
@@ -0,0 +1,4 @@
+# Get input
+name = input("What is your name? ")
+# Print greeting message within sting concatenation
+print("Hi " + name)
---
+++ Assignments/2.ask_two_questions.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+fav_color = input("What is your favourite color? ")
+
+print("Hi I am " + name + " and my fav color is " + fav_color)

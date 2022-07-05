75d138ba27844b386114e52cea799471ceb90eeb
xtremist96 <riz1.xtreme@gmail.com>
2022-07-05 14:42:03 +0530

    Input

---
+++ 4.age.py
@@ -0,0 +1,10 @@
+# Get input and store in variable
+birth_year = input("Birth year: ")
+# Print the type of birth_year
+print(type(birth_year))
+# def age variable and calculate
+age = 2022 - int(birth_year) # Convert birth_year from str to int
+# print age variable
+print(age)
+# Print the type of age
+print(type(age))

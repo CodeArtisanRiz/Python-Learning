36dce13a495778f5fa7cc837cbe6c1d4d77258a3
xtremist96 <riz1.xtreme@gmail.com>
2022-07-05 14:00:36 +0530

    Variables

---
+++ 2.variables.py
@@ -0,0 +1,19 @@
+# Define variables
+# int
+price = 10
+# float
+rating = 4.9
+# string
+name = "Rizwan"
+# boolean
+is_online = False
+#Note: True & False are special keywords in the language
+
+# Update the value of the variable
+price = 20
+# Print the variables
+print(price)
+print(rating)
+print(name)
+print(is_online)
+

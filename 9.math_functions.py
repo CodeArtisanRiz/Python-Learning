fac904941845f5b686e5e5feb22e73ab1f753645
xtremist96 <riz1.xtreme@gmail.com>
2022-07-06 16:58:46 +0530

    Math Functions

---
+++ 9.math_functions.py
@@ -0,0 +1,12 @@
+import math
+# def float variable
+x = 2.9
+# round function
+print(round(x))
+# absolute : returns +ve number
+print(abs(-2.9))
+
+# math module operations
+print(math.ceil(2.9))  # 3
+print(math.floor(2.9)) # 2
+

9b7bbe3508c394af628ffb552bd767e0b1629f8f
xtremist96 <riz1.xtreme@gmail.com>
2022-07-07 02:08:43 +0530

    For Loops

---
+++ 14.for_loops.py
@@ -0,0 +1,23 @@
+# print each character of a word
+for item in "Python":
+    print(item)
+# space
+print("")
+# print list items
+for list in ["Riz","Raj","Shilpa"]:
+    print(list)
+# space
+print("")
+# print range of numbers
+for num_list in range(10):
+    print(num_list)
+# space
+print("")
+# print specific range of numbers
+for num_list in range(5, 10):
+    print(num_list)
+# space
+print("")
+# print specific range of numbers with step
+for num_list in range(5, 10, 2):
+    print(num_list)

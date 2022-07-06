ebf72dea0b17f9e8e2ac3197568f7d785fa47b08
xtremist96 <riz1.xtreme@gmail.com>
2022-07-06 22:45:58 +0530

    typo fixed

--- Assignments/6.weight_converter.py
+++ Assignments/6.weight_converter.py
@@ -7,9 +7,9 @@ if type(weight) != int or float:
     print("Enter a valid number.")
 else:
     if weight_type.upper() == "L":
-        converted_weight = float(weight) * 0.5 # converted_weight is converted from str to float
+        converted_weight = float(weight) * 0.45 # converted_weight is converted from str to float
     elif weight_type.upper() == "K":
-        converted_weight = float(weight) / 0.5  # converted_weight is converted from str to float
+        converted_weight = float(weight) / 0.45  # converted_weight is converted from str to float
     else:
         converted_weight = "Error.."
-    print(converted_weight)
+    print(f"You are {converted_weight}")

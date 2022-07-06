cf7e76e1a55de367fea9e122a51644fc9e15eede
xtremist96 <riz1.xtreme@gmail.com>
2022-07-06 12:17:34 +0530

    String Methods

---
+++ 7.string_methods.py
@@ -0,0 +1,16 @@
+# def str variable
+course = 'Python for Beginners'
+# count lenth of str & print
+print(len(course))
+# covert the entire string to uppercase
+print(course.upper())
+# covert the entire string to lowercase
+print(course.lower())
+# covert the entire string to title format (capitalize first letter of every word)
+print(course.title())
+# find method to find a character or group of them
+print(course.find('P')) #find method is case sensitive
+# replace method to replace a character or group of them with another
+print(course.replace('Beginners', 'Absolute Beginners'))
+# in operator returns a boolean operator
+print('Python' in course)

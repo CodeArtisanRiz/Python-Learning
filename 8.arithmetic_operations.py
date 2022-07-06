98a19669ca4886b644810612e957545c814a9e7b
xtremist96 <riz1.xtreme@gmail.com>
2022-07-06 12:47:41 +0530

    Arithmetic Operations

---
+++ 8.arithmetic_operations.py
@@ -0,0 +1,22 @@
+# Addition
+print(10 + 3)
+# Substraction
+print(10 - 3)
+# Division with ans as float
+print(10 / 3)
+# Division with ans as int
+print(10 // 3)
+# Remainder
+print(10 % 3)
+# Power
+print(3 ** 3)
+
+# Augmented operator
+x = 10
+
+# Addition
+x += 3
+print(x)
+# Substraction
+x -= 3
+print(x)

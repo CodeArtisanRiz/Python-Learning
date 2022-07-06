055e573894f4e16f08a1ac9f51727ae0a8fd64e0
xtremist96 <riz1.xtreme@gmail.com>
2022-07-06 22:14:43 +0530

    comparison Operators

---
+++ 12.comparison_operators.py
@@ -0,0 +1,14 @@
+# def variable
+temp = 30
+if temp == 35:
+    print("It's really hot today")
+elif temp != 35:
+    print("Its quite hot today")
+elif temp > 30:
+    print("It's hot today")
+elif temp >= 30:
+    print("It's not so hot day")
+elif temp < 30:
+    print("It's cold today")
+else:
+    print("Neither")

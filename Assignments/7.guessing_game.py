af62821088927210a64a1e9a2552a8cda18f234d
xtremist96 <riz1.xtreme@gmail.com>
2022-07-06 23:55:05 +0530

    Guessing Game

---
+++ Assignments/7.guessing_game.py
@@ -0,0 +1,14 @@
+secret_number = 6
+guess_count = 0
+guess_limt = 3
+while guess_count < guess_limt:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won..!")
+        break
+    else:
+        print("try again..")
+else:
+    print("You failed...")
+print("Game ended...")

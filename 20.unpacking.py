001fc175a4f4ec40252c0c3913949c73470e5d99
xtremist96 <riz1.xtreme@gmail.com>
2022-07-08 00:34:35 +0530

    Unpacking

--- 20.unpacking.py
+++ 20.unpacking.py
@@ -1,10 +1,14 @@
 # def a tuple
-coordinates = (1, 2, 3)
+coordinates_tuple = (1, 2, 3)
+# def a list
+coordinates_list = [1, 2, 3]
 # unpacking
-x, y, z = coordinates
+a, b, c = coordinates_tuple
+x, y, z = coordinates_list
 # it will act just like:
 #     x = coordinates[0]
 #     y = coordinates[1]
 #     z = coordinates[2]
 
+print(f'a: {a} b: {b} c: {c}')
 print(f'x: {x} y: {y} z: {z}')

d947393300c5b50079a75e3696fc28527b58ad6e
xtremist96 <riz1.xtreme@gmail.com>
2022-07-08 21:32:39 +0530

    Emoji Converter using dictionary

---
+++ Assignments/15.emoji_converter.py
@@ -0,0 +1,13 @@
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)": "😊",
+    ";)": "😉",
+    ":(": "😔",
+    ";(": "😢",
+    ":D": "😁"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)

a8551897d62eef7ca2c89e723cc7c31fe75e826f
xtremist96 <riz1.xtreme@gmail.com>
2022-07-09 12:08:07 +0530

    Parameters

---
+++ 23.parameters.py
@@ -0,0 +1,9 @@
+# def function
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}')
+
+
+# call function & pass parameter
+greet_user("John", "Smith")
+greet_user("John", "Doe")
+greet_user("Alex", "Dunphy")

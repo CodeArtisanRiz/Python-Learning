cbb821687730d6a5b10c25defd2e86853e0d4789
xtremist96 <riz1.xtreme@gmail.com>
2022-07-09 13:12:02 +0530

    Parameter positioning with keyword argument

---
+++ 24.keyword_arguments.py
@@ -0,0 +1,9 @@
+# def function
+def greet_user(first_name, last_name):
+    # print formatted sting with passed parameters
+    print(f'Hi {first_name} {last_name}')
+
+
+# call function & pass parameter
+# parameter positioning with keyword argument
+greet_user(last_name="Smith", first_name="John")
